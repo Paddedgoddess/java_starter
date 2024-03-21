c4f849cd9cb780484bb76c5e208a4679e4869a7b
Paddedgoddess <159901446+Paddedgoddess@users.noreply.github.com>
2024-03-21 12:00:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# java_starter
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Paddedgoddess/java_starter)
---
+++ script.js
@@ -0,0 +1,18 @@
+console.log('beep boop');
+
+let age = 24;
+console.log(age);
+console.log(typeof age);
+
+const userName = 'Emilie';
+console.log(userName);
+console.log(typeof userName);
+
+const greet = 'Hallais';
+console.log(greet);
+
+
+
+
+const greetPerson = `${greet}, ${userName}!`;
+console.log(greetPerson);
